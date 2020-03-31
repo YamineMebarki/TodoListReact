6f98e9759aaaf2bc7f4e7a913fc72e404e8d8e91
Mebarki <yamine@yamine-GE62VR-6RF>
2020-03-31 14:13:04 +0200

    Updates

---
+++ precache-manifest.1740c34e385482710d2f87228a97ab4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0266cb25b7dc265e7be2ebc573124670",
+    "url": "/TodoListReact/index.html"
+  },
+  {
+    "revision": "5180845498894878c49f",
+    "url": "/TodoListReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0f35fdcff101356772e",
+    "url": "/TodoListReact/static/js/2.5e956fa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoListReact/static/js/2.5e956fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5180845498894878c49f",
+    "url": "/TodoListReact/static/js/main.0ddbed71.chunk.js"
+  },
+  {
+    "revision": "5d361bf419fd6a0cf32e",
+    "url": "/TodoListReact/static/js/runtime-main.c6fec50e.js"
+  }
+]);
